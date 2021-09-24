4e20e3e2280106c17b650ba075d733f17eee95cb
feixes <xavierfeixesguell@gmail.com>
2021-09-25 01:50:51 +0200

    dise;o menu superior

--- CRUD.py
+++ CRUD.py
@@ -2,6 +2,35 @@ from tkinter import *
 from tkinter import messagebox
 from sqlite3 import *
 
-root=Tk()
-# this is just to try if i can commit to git
+root = Tk()
+
+barraMenu = Menu(root)
+root.config(menu=barraMenu, width=300, height=300)
+
+# Creamos cada elemento del menu con las opciones de los desplegables
+bbddMenu = Menu(barraMenu, tearoff=0)
+bbddMenu.add_command(label="Conectar")
+bbddMenu.add_command(label="Salir")
+
+borrarMenu = Menu(barraMenu, tearoff=0)
+borrarMenu.add_command(label="Borrar")
+
+crudMenu = Menu(barraMenu, tearoff=0)
+crudMenu.add_command(label="Crear")
+crudMenu.add_command(label="Leer")
+crudMenu.add_command(label="Actualizar")
+crudMenu.add_command(label="Borrar")
+
+ayudaMenu = Menu(barraMenu, tearoff=0)
+ayudaMenu.add_command(label="Licencia")
+ayudaMenu.add_command(label="Acerca de...")
+
+# Creamos las etiquetas de cada manu
+barraMenu.add_cascade(label="BBDD", menu=bbddMenu)
+barraMenu.add_cascade(label="Borrar", menu=borrarMenu)
+barraMenu.add_cascade(label="CRUD", menu=crudMenu)
+barraMenu.add_cascade(label="Ayuda", menu=ayudaMenu)
+
+# Construcción de campos grid
+
 root.mainloop()
