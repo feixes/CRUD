23e48829c13b4eb27606af30f1838b881a88a451
feixes <xavierfeixesguell@gmail.com>
2021-09-26 19:33:08 +0200

    función delete

--- CRUD.py
+++ CRUD.py
@@ -38,6 +38,8 @@ def salir_beber_el_rollo_de_siempre():
     if valor_salir == "yes":
         root.destroy()
 
+# ---------------------------- Función borrar campos
+
 
 def borrar_campos():
     # no le puedo asignar directamente una cadena vacía (mi_id="") porque son variables de control
@@ -48,6 +50,8 @@ def borrar_campos():
     mi_direccion.set("")
     # para el texto, desde el principio 1.0, hasta el final END
     textoComentario.delete(1.0, END)
+
+# ---------------------------- Funciones CRUD
 
 
 def crear():
@@ -92,6 +96,15 @@ def update():
     messagebox.showinfo("BBDD", "Registro actualizado con éxito")
 
 
+def delete():
+    mi_conexion = sqlite3.connect("Usuarios")
+    mi_cursor = mi_conexion.cursor()
+
+    mi_cursor.execute(f"DELETE FROM datos_usuarios WHERE id={mi_id.get()}")
+    mi_conexion.commit()
+    messagebox.showinfo("BBDD", "Registro borrado correctamente")
+
+
 # ---------------------- Variables de control
 mi_id = StringVar()
 mi_nombre = StringVar()
@@ -115,7 +128,7 @@ crudMenu = Menu(barraMenu, tearoff=0)
 crudMenu.add_command(label="Create", command=crear)
 crudMenu.add_command(label="Read", command=read)
 crudMenu.add_command(label="Update", command=update)
-crudMenu.add_command(label="Delete")
+crudMenu.add_command(label="Delete", command=delete)
 
 ayudaMenu = Menu(barraMenu, tearoff=0)
 ayudaMenu.add_command(label="Licencia")
@@ -187,7 +200,7 @@ botonRead.grid(row=0, column=1, padx=10, pady=10)
 botonUpdate = Button(frameBotones, text="Update", command=update)
 botonUpdate.grid(row=0, column=2, padx=10, pady=10)
 
-botonDelete = Button(frameBotones, text="Delete")
+botonDelete = Button(frameBotones, text="Delete", command=delete)
 botonDelete.grid(row=0, column=3, padx=10, pady=10)
 
 
